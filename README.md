cc86cda30226d9b41b39ddfcbe456e583e0f0b59
vahdet <vahdetkeskin@gmail.com>
2021-05-30 22:45:23 +0300

    add initial stuff

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-express-skeleton
+
 A basic skeleton project for NodeJS (TypeScript, Jest...)
+
+## Run locally
+
+Run the follows on project root. It runs nodemon as noted in `scripts` section of `package.json` .
+
+```sh
+npm start
+```
---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
+import { defaultsESM as tsjPreset } from 'ts-jest/presets'
+// import { defaults as tsjPreset } from 'ts-jest/presets'
+// import { jsWithTs as tsjPreset } from 'ts-jest/presets'
+// import { jsWithTsESM as tsjPreset } from 'ts-jest/presets'
+// import { jsWithBabel as tsjPreset } from 'ts-jest/presets'
+// import { jsWithBabelESM as tsjPreset } from 'ts-jest/presets'
+
+const config: InitialOptionsTsJest = {
+  transform: {
+    ...tsjPreset.transform
+    // [...]
+  }
+}
+
+export default config
